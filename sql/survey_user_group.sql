b2aab73972587b0db3c467a5f4acd20425f605bd
D073980 <robin.herder@sap.com>
2020-04-07 11:44:02 +0200

    Removed obsolete sql files

--- sql/survey_user_group.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS survey_schema DEFAULT CHARACTER SET utf8;
-USE survey_schema;
-DROP TABLE IF EXISTS survey_schema.survey_user_group;
-
-CREATE TABLE survey_schema.survey_user_group(
-    course_short    VARCHAR(8)  PRIMARY KEY,
-    course          VARCHAR(50) NOT NULL,   
-)
