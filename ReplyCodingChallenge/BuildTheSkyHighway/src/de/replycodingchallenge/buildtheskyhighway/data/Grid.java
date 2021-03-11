37ece33c9325d3e1d00f8353b6c936332a8e1fc4
malik <malik4privat@gmail.com>
2021-03-11 19:36:32 +0100

    push

--- ReplyCodingChallenge/BuildTheSkyHighway/src/de/replycodingchallenge/buildtheskyhighway/data/Grid.java
+++ ReplyCodingChallenge/BuildTheSkyHighway/src/de/replycodingchallenge/buildtheskyhighway/data/Grid.java
@@ -1,18 +1,30 @@
 package de.replycodingchallenge.buildtheskyhighway.data;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Grid {
     private final int width;
     private final int height;
     private final int reward;
     private final int presentBuilding;
     private final int availableAntennas;
+    private Antenna[] antennas;
+    private Building[] buildings;
+    private char[][] grid;
 
-    public Grid (int width, int height, int reward, int presentBuilding, int availableAntennas) {
+    public Grid (int width, int height, int reward, int presentBuilding, int availableAntennas, ArrayList<String> data) {
         this.width = width;
         this.height = height;
         this.reward = reward;
         this.presentBuilding = presentBuilding;
         this.availableAntennas = availableAntennas;
+        this.buildings = new Building[getPresentBuilding()];
+        this.antennas = new Antenna[getAvailableAntennas()];
+        setBuildings(data);
+        setAntennas(data);
+        this.grid = new char[height][width];
+        setBuildingsToGrid();
     }
 
     public int getWidth() {
@@ -33,5 +45,46 @@ public class Grid {
 
     public int getAvailableAntennas() {
         return availableAntennas;
+    }
+
+    public Antenna[] getAntennas() {
+        return antennas;
+    }
+
+    public Building[] getBuildings() {
+        return buildings;
+    }
+
+    public char[][] getGrid() {
+        return grid;
+    }
+
+    public void setBuildings(ArrayList<String> data) {
+        this.buildings = new Building[getPresentBuilding()];
+        for (int i = 0; i < getPresentBuilding(); i++) {
+            String[] values = data.get(i + 2).split("\\s+");
+            buildings[i] = new Building(new Point(Integer.parseInt(values[0]),Integer.parseInt(values[1])), Integer.parseInt(values[2]), Integer.parseInt(values[3]));
+        }
+    }
+
+    public void setAntennas(ArrayList<String> data) {
+        this.antennas = new Antenna[getAvailableAntennas()];
+        for (int i = 0; i < getAvailableAntennas(); i++) {
+            String[] values = data.get(i + getPresentBuilding() + 2).split("\\s+");
+            antennas[i] = new Antenna(i, Integer.parseInt(values[0]), Integer.parseInt(values[1]));
+        }
+    }
+
+    public void setBuildingsToGrid() {
+        for (Building building : buildings) {
+            grid[(int)building.getCoordination().getY()][(int)building.getCoordination().getX()] = 'B';
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] != 'B') {
+                    grid[i][j] = 'N';
+                }
+            }
+        }
     }
 }
--- ReplyCodingChallenge/BuildTheSkyHighway/src/de/replycodingchallenge/buildtheskyhighway/logic/Calculator.java
+++ ReplyCodingChallenge/BuildTheSkyHighway/src/de/replycodingchallenge/buildtheskyhighway/logic/Calculator.java
@@ -1,4 +1,40 @@
 package de.replycodingchallenge.buildtheskyhighway.logic;
 
+import de.replycodingchallenge.buildtheskyhighway.data.Antenna;
+import de.replycodingchallenge.buildtheskyhighway.data.Building;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Calculator {
+
+    public int fullScore(Building[] buildings, Antenna[] antennas, int reward) {
+        int score = 0;
+        boolean areAllBuildingsAssigned = true;
+        for (int i = 0; i < buildings.length; i++) {
+            HashMap<Integer, Integer> scores = new HashMap<>();
+            for (int j = 0; j < antennas.length; j++) {
+                int distance = (int)Math.abs(buildings[i].getCoordination().getX()-antennas[j].getCoordination().getX()) + (int)Math.abs(buildings[i].getCoordination().getY()-antennas[j].getCoordination().getY());
+                if (distance <= antennas[i].getRange()) {
+                    int value = (buildings[i].getConnectionSpeedWeight()*antennas[j].getConnectionSpeed()) - (buildings[i].getLatencyWeight()*distance);
+                    scores.put(j,value);
+                }
+            }
+            if (!scores.isEmpty()) {
+                Map.Entry<Integer, Integer> maxEntry = null;
+                for (Map.Entry<Integer, Integer> entry : scores.entrySet()) {
+                    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                        maxEntry = entry;
+                    }
+                }
+                score += maxEntry.getValue();
+            } else {
+                areAllBuildingsAssigned = false;
+            }
+        }
+        if (areAllBuildingsAssigned == true) {
+            score += reward;
+        }
+        return score;
+    }
 }
